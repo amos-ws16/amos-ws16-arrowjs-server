367f7c936a86c8c62b3f23c0056784304e87c026
Oliver Esser <oliver.esser@campus.tu-berlin.de>
2016-11-04 11:35:39 +0100

    Refactor supertest to use async syntax.

--- test/server-test.js
+++ test/server-test.js
@@ -1,18 +1,16 @@
 let buster = require('buster')
-let request = require('supertest')
+let request = require('supertest-as-promised')
 let app = require('../lib/server.js')
 
 buster.testCase('GET /api/welcome', {
-  'should return welcome message': (done) => {
-    request(app)
+  'should return welcome message': async () => {
+    let res = await request(app)
       .get('/api/welcome')
       .expect('Content-Type', /json/)
       .expect(200)
-      .end(done((err, res) => {
-        buster.refute(err)
-        buster.assert(res.body.message)
-        buster.assert.match(res.body.message, /Welcome/)
-      }))
+
+    buster.assert(res.body.message)
+    buster.assert.match(res.body.message, /Welcome/)
   }
 })
 
