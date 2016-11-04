c802e9a4834403f4ffb9b281bc1626bc7a97f287
Oliver Esser <oliver.esser@campus.tu-berlin.de>
2016-11-04 12:10:55 +0100

    Return trivial auth token on GET /api/auth.

--- test/server-test.js
+++ test/server-test.js
@@ -2,15 +2,16 @@ let buster = require('buster')
 let request = require('supertest-as-promised')
 let app = require('../lib/server.js')
 
-buster.testCase('GET /api/welcome', {
-  'should return welcome message': async () => {
+buster.testCase('GET /api/auth', {
+  'should return a auth token': async () => {
     let res = await request(app)
-      .get('/api/welcome')
+      .get('/api/auth')
       .expect('Content-Type', /json/)
       .expect(200)
 
-    buster.assert(res.body.message)
-    buster.assert.match(res.body.message, /Welcome/)
+    let message = res.body
+    buster.assert(message.success)
+    // Any string is fine for now.
+    buster.assert.isString(message.token)
   }
 })
-
