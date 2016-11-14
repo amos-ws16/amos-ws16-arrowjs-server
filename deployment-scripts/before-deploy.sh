9441e2c26e2c2427fe75a2ecc3607a2898e50603
Simon Schwan <simonschwan@Simons-MacBook-Pro.local>
2016-11-14 17:29:24 +0100

    changed permission of before-deploy.sh script

--- deployment-scripts/before-deploy.sh
+++ deployment-scripts/before-deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo 'this script is being executed before deployment\n'
-echo 'it changes the behaviour depending on the branch (master|dev)'
+echo 'it changes the behaviour depending on the branch (master|devg)'
 echo $TRAVIS_BRANCH
