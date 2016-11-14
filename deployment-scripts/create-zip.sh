b2c60623b8c4e7d48df922d5d55eab804c756ea9
Simon Schwan <simonschwan@Simons-MacBook-Pro.local>
2016-11-14 16:46:14 +0100

    updated create-zip.sh

--- deployment-scripts/create-zip.sh
+++ deployment-scripts/create-zip.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir deployment-build
 mkdir deployment-build/temp
 cp -R bin deployment-scripts lib test .eslintignore .eslintrc appspec.yml LICENSE.md package.json README.md deployment-build/temp/
 cd deployment-build/temp
