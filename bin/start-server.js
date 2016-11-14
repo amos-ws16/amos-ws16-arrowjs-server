427091f5920dc6428a98df5382f84c38433c91b8
Oliver Esser <oliver.esser@campus.tu-berlin.de>
2016-11-14 22:46:03 +0100

    Add configuration file and refactor modules to use it.
    The ScoreManager, plugins and aggregator are now configured in
    config/index.js and all server reated logic has been moved to the
    lib/server/ directory.

--- bin/start-server.js
+++ bin/start-server.js
@@ -1,15 +1,8 @@
 'use strict'
 
 const server = require('../lib/server')
-const ScoreManager = require('../lib/score-manager')
-const aggregator = require('../lib/score-aggregator')
-const sameTitlePlugin = require('../lib/plugins/same-title-plugin')
 
 const port = process.env.PORT || 3000
-
-let manager = new ScoreManager(new aggregator.Mean())
-manager.registerPlugin('same-title', sameTitlePlugin)
-server.setScoreManager(manager)
 
 server.listen(port, () => {
   console.log(`Server started on Port: ${port}`)
---
+++ config/index.js
@@ -0,0 +1,19 @@
+const ScoreManager = require('../lib/score-manager')
+const aggregator = require('../lib/score-aggregator')
+const sameTitlePlugin = require('../lib/plugins/same-title-plugin')
+
+/**
+ * Returns a new ScoreManager where the default plugins
+ * and aggregator are set up.
+ */
+function makeScoreManager () {
+  let manager = new ScoreManager(new aggregator.Mean())
+  manager.registerPlugin('same-title', sameTitlePlugin)
+  return manager
+}
+
+let config = {}
+config.scoreManager = makeScoreManager()
+config.secretToken = '8cd96c8697d12daf4dfd135aec01fd63ee058ab4'
+
+module.exports = config
