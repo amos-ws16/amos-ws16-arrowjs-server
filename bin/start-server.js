7bdbcb49f9a149e4b8915b930d77c7604a3c18ae
Oliver Esser <oliver.esser@campus.tu-berlin.de>
2016-11-14 13:56:11 +0100

    Add default ScoreManager in cli application.

--- bin/start-server.js
+++ bin/start-server.js
@@ -1,7 +1,15 @@
 'use strict'
 
 const server = require('../lib/server')
+const ScoreManager = require('../lib/score-manager')
+const ScoreCombiner = require('../lib/score-combiner')
+const sameTitlePlugin = require('../lib/plugins/same-title-plugin')
+
 const port = process.env.PORT || 3000
+
+let manager = new ScoreManager(new ScoreCombiner.Mean())
+manager.registerPlugin('same-title', sameTitlePlugin)
+server.setScoreManager(manager)
 
 server.listen(port, () => {
   console.log(`Server started on Port: ${port}`)
