dfa0a6cf6fe2b189a0621dbcd7b2d80d2e25c0a0
Simon Schwan <simonschwan@wlan-141-23-141-207.tubit.tu-berlin.de>
2016-11-15 09:58:47 +0100

    changed configuration to work on master and dev branch

--- deployment-scripts/before-deploy.sh
+++ deployment-scripts/before-deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo 'change the behaviour depending on the branch (master|dev)'
-if [ "$TRAVIS_BRANCH" = "feature-32-travis-deployment-master" ]; then
+if [ "$TRAVIS_BRANCH" = "master" ]; then
   echo 'deploy on master'
   cp deployment-scripts/config-master.cfg deployment-scripts/config.cfg
   cp appspec-master.yml appspec.yml
