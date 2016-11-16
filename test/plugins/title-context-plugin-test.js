cbd7a558e1f2147245bd20a7fae3ade731744628
Duc <nguyen.duc@googlemail.com>
2016-11-16 16:17:24 +0100

    added tests for using the similarity lib and using the right arguments

--- test/plugins/title-context-plugin-test.js
+++ test/plugins/title-context-plugin-test.js
@@ -1,10 +1,10 @@
 const buster = require('buster')
-const utils = require('../../lib/utils.js')
 const titleContextPlugin = require('../../lib/plugins/title-context-plugin.js')
 const stringSimilarity = require('string-similarity')
 
 buster.testCase('titleContextPlugin', {
   'should return the same value as Stringsimilarity compare method': function () {
+    this.stub(stringSimilarity, 'compareTwoStrings').returns(1.0)
     let file = {
       title: 'healed.jpeg'
     }
@@ -13,6 +13,32 @@ buster.testCase('titleContextPlugin', {
     }
 
     let result = titleContextPlugin(file, task)
-    buster.assert.equals(result, stringSimilarity.compareTwoStrings(utils.basename(file.title), task.title))
+    buster.assert.near(result, 1.0, 1e-3)
+  },
+
+  'should call Stringsimilarity compare method': function () {
+    let compare = this.stub(stringSimilarity, 'compareTwoStrings')
+    let file = {
+      title: 'healed.jpeg'
+    }
+    let task = {
+      title: 'sealed'
+    }
+
+    titleContextPlugin(file, task)
+    buster.assert.called(compare)
+  },
+
+  'should pass two strings for Stringsimilarity compare method': function () {
+    let compare = this.stub(stringSimilarity, 'compareTwoStrings')
+    let file = {
+      title: 'healed.jpeg'
+    }
+    let task = {
+      title: 'sealed'
+    }
+
+    titleContextPlugin(file, task)
+    buster.assert.calledWith(compare, 'healed', 'sealed')
   }
 })
