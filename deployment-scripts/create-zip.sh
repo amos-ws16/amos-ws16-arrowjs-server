e4e02fd49dbdf2f6a2402e6fbec68f606c15d510
Simon Schwan <simonschwan@wlan-141-23-141-207.tubit.tu-berlin.de>
2016-11-16 16:39:45 +0100

    create-zip.sh with additional files that are not in repo

--- deployment-scripts/create-zip.sh
+++ deployment-scripts/create-zip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir deployment-build
 mkdir deployment-build/temp
-cp -R $(git ls-tree --name-only HEAD) deployment-build/temp/
+cp -R $(git ls-tree --name-only HEAD) deployment-scripts/config.cfg appspec.yml deployment-build/temp/
 cd deployment-build/temp
 zip -r ../latest.zip ./
 cd ../..
