548c0c33396c3cbbf405879026481f9529f9831f
Simon Schwan <simonschwan@Simons-MBP.fritz.box>
2016-11-20 17:01:19 +0100

    created validate script. found mistake in dokumentation - fixed

--- deployment-scripts/create-zip.sh
+++ deployment-scripts/create-zip.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-mkdir deployment-build
-mkdir deployment-build/temp
+mkdir -p deployment-build/temp
 cp -R $(git ls-tree --name-only HEAD) deployment-scripts/config.cfg appspec.yml deployment-build/temp/
 cd deployment-build/temp
 zip -r ../latest.zip ./
 cd ../..
-rm -R deployment-build/temp
