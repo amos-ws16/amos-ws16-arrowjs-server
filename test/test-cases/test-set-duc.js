ce664bbbda2aadbf213f25373adf14db1c229b27
Duc <nguyen.duc@googlemail.com>
2016-11-23 22:29:06 +0100

    testcase added

---
+++ test/test-cases/test-set-duc.js
@@ -0,0 +1,205 @@
+
+let task1 = {
+  name: 'apply for ERASMUS',
+  timestamp: 1479687500,
+  due_date: 1483518000,
+  user: 'kao',
+  assignees: [ 'duc.nguyen', 'jasmin', 'scott' ],
+  description: 'Apply for the winter and summer term ERASMUS program at the TU Berlin.',
+  location: 'Berlin'
+}
+
+let task2 = {
+  name: 'letter of recommendation',
+  timestamp: 1479555500,
+  due_date: 1480398000,
+  user: 'kao',
+  assignees: [ 'duc.nguyen', 'jasmin' ],
+  description: 'For the application of a semester abroad you need letters of recommendation from different faculties.',
+  location: 'Germany'
+}
+
+let task3 = {
+  name: 'Write a CV',
+  timestamp: 1493515800,
+  due_date: 1531864730,
+  user: 'duc.nguyen',
+  description: 'Write a curriculum vitae for job applications, university programs, as an exercise etc.',
+  location: 'no location'
+}
+
+let task4 = {
+  name: 'learn NodeJS',
+  timestamp: 1479391500,
+  due_date: 1479942000,
+  user: 'dirk.riehle',
+  assignees: [ 'duc.nguyen', 'jasmin', 'jason', 'scott' ],
+  description: 'Every AMOS member should create 10 test examples with which we can use to evaluate our scoring mechanism.',
+  location: 'no location'
+}
+
+let task5 = {
+  name: 'visit home',
+  timestamp: 1479362100,
+  due_date: 1479469800,
+  user: 'jason',
+  assignees: [ 'jasmin', 'duc.nguyen', 'kao', 'tai' ],
+  description: 'Visit your mum and dad regularly.',
+  location: 'Dresden'
+}
+
+let task6 = {
+  name: 'exercise',
+  timestamp: 1484412200,
+  due_date: 1485987800,
+  user: 'jason',
+  assignees: [ 'duc.nguyen', 'jasmin' ],
+  description: 'Do some sport to stay fit in your everyday life.'
+}
+
+let task7 = {
+  name: 'do homework',
+  timestamp: 1477336400,
+  due_date: 1478645300,
+  user: 'dirk.riehle',
+  assignees: [ 'duc.nguyen', 'kao', 'tai', 'jasmin', 'jason', 'scott' ],
+  description: 'To pass this semester you have to do homeworks every week. It is recommended to solve all tasks for a good grade.',
+  location: 'no location'
+}
+
+let task8 = {
+  name: 'do chores',
+  timestamp: 1479663200,
+  due_date: 1480316150,
+  user: 'duc.nguyen',
+  assignees: [ 'kao', 'duc.nguyen', 'tai', 'jasmin', 'jason', 'dirk.riehle', 'scott' ]
+}
+
+let task9 = {
+  name: 'pay bills',
+  timestamp: 1479654800,
+  due_date: 1479545600,
+  user: 'scott'
+}
+
+let task10 = {
+  name: 'university project',
+  timestamp: 1473216680,
+  due_date: 1479523130,
+  user: 'dirk.riehle',
+  assignees: [ 'duc.nguyen', 'jasmin', 'jason', 'scott', 'dirk.riehle' ],
+  description: 'The new AMOS project during the winter term 2016/2017 is about writing an NodeJS application for an external company with functionality of scoring similiarities in tasks and files inside an chat.'
+}
+
+let testCase1 = {
+  tasks: [task1, task2, task3],
+  file: {
+    name: 'study program abroad',
+    filetype: 'pdf',
+    timestamp: 1488877310,
+    user: 'duc.nguyen'
+  }
+}
+
+let testCase2 = {
+  tasks: [task4, task6, task7, task10],
+  file: {
+    name: 'winter term 2016',
+    filetype: 'docx',
+    timestamp: 1477256232,
+    user: 'scott'
+  }
+}
+
+let testCase3 = {
+  tasks: [task4, task5, task6, task7, task8],
+  file: {
+    name: 'everyday tasks',
+    filetype: 'jpeg',
+    timestamp: 1488883310,
+    user: 'jason'
+  }
+}
+
+let testCase4 = {
+  tasks: [task4, task6, task7, task10],
+  file: {
+    name: 'computer science',
+    filetype: 'doc',
+    timestamp: 1483554331,
+    user: 'dirk.riehle'
+  }
+}
+
+let testCase5 = {
+  tasks: [task1, task2, task3, task4, task6, task7, task8, task10],
+  file: {
+    name: 'future investment',
+    filetype: 'png',
+    timestamp: 1488668864,
+    user: 'jasmin'
+  }
+}
+
+let testCase6 = {
+  tasks: [task1, task2, task3],
+  file: {
+    name: 'computer science project',
+    filetype: 'xml',
+    timestamp: 1483135483,
+    user: 'duc.nguyen'
+  }
+}
+
+let testCase7 = {
+  tasks: [task1, task2, task3, task5, task7],
+  file: {
+    name: 'university and homework',
+    filetype: 'zip',
+    timestamp: 1478926233,
+    user: 'kao'
+  }
+}
+
+let testCase8 = {
+  tasks: [task7, task8],
+  file: {
+    name: 'sport',
+    filetype: 'rar',
+    timestamp: 1480883310,
+    user: 'jason'
+  }
+}
+
+let testCase9 = {
+  tasks: [task2, task3, task4],
+  file: {
+    name: 'job interview',
+    filetype: 'pdf',
+    timestamp: 1476593520,
+    user: 'kao'
+  }
+}
+
+let testCase10 = {
+  tasks: [task7, task8, task9],
+  file: {
+    name: 'parents and home',
+    filetype: 'docx',
+    timestamp: 1485684310,
+    user: 'tai'
+  }
+}
+
+module.exports = {
+  testCase1,
+  testCase2,
+  testCase3,
+  testCase4,
+  testCase5,
+  testCase6,
+  testCase7,
+  testCase8,
+  testCase9,
+  testCase10
+}
