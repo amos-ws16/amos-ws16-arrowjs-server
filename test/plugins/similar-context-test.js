613b3599c75ada61d84ed6b9f43c5efde95303ff
Duc <nguyen.duc@googlemail.com>
2016-11-27 13:01:38 +0100

    differentiate exceptions, changed test description

--- test/plugins/similar-context-test.js
+++ test/plugins/similar-context-test.js
@@ -2,14 +2,21 @@ const buster = require('buster')
 const plugin = require('../../lib/plugins/similar-context-plugin.js')
 
 buster.testCase('similar-context-plugin', {
-  'should return 1.0 when the keywords of file description and task description match exactly': function () {
+  'without Keywordsextraction: should return 1.0 when both strings matches': function () {
     let sString1 = 'president obama woke monday facing congressional defeat parties believed hobble presidency'
     let sString2 = 'President Obama woke up Monday facing a Congressional defeat that many in both parties believed could hobble his presidency.'
     let result = plugin(sString1, sString2, true)
     buster.assert.near(1.0, result, 0)
   },
 
-  'should return 0.0 when the keywords of file description and task description dont match at all': function () {
+  'with Keywordsextraction: should return 1.0 when the keywords of file description and task description match exactly': function () {
+    let sString1 = 'president obama woke monday facing congressional defeat parties believed hobble presidency'
+    let sString2 = 'President Obama woke up Monday facing a Congressional defeat that many in both parties believed could hobble his presidency.'
+    let result = plugin(sString1, sString2, true)
+    buster.assert.near(1.0, result, 0)
+  },
+
+  'with Keywordsextraction: should return 0.0 when the keywords of file description and task description dont match at all': function () {
     let sString1 = '123 3415i 02387564'
     let sString2 = 'President Obama woke up Monday facing a Congressional defeat that many in both parties believed could hobble his presidency.'
     let result = plugin(sString1, sString2, true)
