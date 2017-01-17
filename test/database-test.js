887ec28ee9cc1d1f5563890e6f1f54345c80059a
Simon Schwan <simonschwan@Simons-MacBook-Pro.local>
2017-01-17 18:09:43 +0100

    updated database. works now with tests and integration to server

--- test/database-test.js
+++ test/database-test.js
@@ -3,15 +3,16 @@ var mongoose = require('mongoose')
 var dbLib = require('../lib/database')
 var dbConfig = require('../../db-config.js')
 
+var db = null
+
 /**
  * Test cases which check the functionality of the database library.
  */
 buster.testCase('database-library-test', {
   setUp: function (done) {
     // connect and wait for established connection
-    dbLib.connect(dbConfig.test)
-    var db = mongoose.connection
-    db.once('open', () => {
+    db = dbLib.connect(dbConfig.test)
+    db.on('connected', () => {
       // delete users database
       dbLib.model.User.remove({}, (err) => {
         if (err) {
@@ -28,9 +29,8 @@ buster.testCase('database-library-test', {
     })
   },
   tearDown: function (done) {
-    mongoose.disconnect()
-    var db = mongoose.connection
-    db.once('close', done(() => {}))
+    db.close()
+    db.on('disconnected', done(() => {}))
   },
   'test-user-is-valid': function (done) {
     dbLib.isUserValid('realuser', 'secret', done((valid) => {
@@ -44,8 +44,7 @@ buster.testCase('database-library-test', {
   },
   'test-user-is-not-valid-if-error': function () {
     // TODO: How to check this
-    /* mongoose.disconnect()
-    dbLib.isUserValid(null, null, done((valid) => {
+    /* dbLib.isUserValid(null, null, done((valid) => {
       buster.assert(!valid)
     })) */
     buster.assert(true)
@@ -99,55 +98,41 @@ buster.testCase('database-library-test', {
  * Test cases which check the connection to the databases
  */
 buster.testCase('database-connection-test', {
+  tearDown: function (done) {
+    db.close()
+    db.on('disconnected', done(() => {}))
+  },
   'test-connection-to-test-db': function (done) {
-    mongoose.connect(dbConfig.test)
-    var db = mongoose.connection
+    db = mongoose.createConnection(dbConfig.test)
 
-    db.once('error', done(() => {
+    db.on('error', done(() => {
       buster.assert(false)
     }))
 
-    db.once('open', () => {
+    db.on('connected', done(() => {
       buster.assert(true)
-      mongoose.disconnect()
-    })
-
-    db.once('close', done(() => {
-      // console.log('this tells me that i am DISCONNECTED from buster')
     }))
   },
   'test-connection-to-dev-db': function (done) {
-    mongoose.connect(dbConfig.dev)
-    var db = mongoose.connection
+    db = mongoose.createConnection(dbConfig.dev)
 
-    db.once('error', done(() => {
+    db.on('error', done(() => {
       buster.assert(false)
     }))
 
-    db.once('open', () => {
+    db.on('connected', done(() => {
       buster.assert(true)
-      mongoose.disconnect()
-    })
-
-    db.once('close', done(() => {
-      // console.log('this tells me that i am DISCONNECTED from buster')
     }))
   },
   'test-connection-to-prod-db': function (done) {
-    mongoose.connect(dbConfig.prod)
-    var db = mongoose.connection
+    db = mongoose.createConnection(dbConfig.prod)
 
-    db.once('error', done(() => {
+    db.on('error', done(() => {
       buster.assert(false)
     }))
 
-    db.once('open', () => {
+    db.on('connected', done(() => {
       buster.assert(true)
-      mongoose.disconnect()
-    })
-
-    db.once('close', done(() => {
-      // console.log('this tells me that i am DISCONNECTED from buster')
     }))
   }
 })
