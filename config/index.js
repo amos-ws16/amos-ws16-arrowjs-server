204a63bfcb581f5c845f2fabe3582d0307d40b74
Oliver Esser <oliver.esser@campus.tu-berlin.de>
2017-01-18 22:56:58 +0100

    Plug new route handler into app.

--- config/index.js
+++ config/index.js
@@ -1,6 +1,5 @@
 const config = {}
 
-config.secretToken = '8cd96c8697d12daf4dfd135aec01fd63ee058ab4'
 config.token = {
   secret: '8cd96c8697d12daf4dfd135aec01fd63ee058ab4',
   expiresInMinutes: 1440 // 24 hours
