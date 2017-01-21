0caacf45b5869d02c37086ec3486dd05d8ee7189
Oliver Esser <oliver.esser@campus.tu-berlin.de>
2017-01-21 09:47:21 +0100

    Remove database driver.
    It is superseded by models in lib/models/*.js and thus no longer needed.

--- bin/start-server.js
+++ bin/start-server.js
@@ -3,13 +3,14 @@
 const mongoose = require('mongoose')
 
 const server = require('../lib')
-const config = require('../config')
+const dbConfig = require('../../db-config')
 
 const cliPort = process.argv.length > 2 && process.argv[2] || undefined
 const port = cliPort || process.env.PORT || 3000
 
 mongoose.Promise = global.Promise
-mongoose.connect(config.database)
+const db = process.env.NODE_ENV === 'production' ? dbConfig.prod : dbConfig.dev
+mongoose.connect(db)
 
 server.listen(port, () => {
   console.log(`Server started on Port: ${port}`)
