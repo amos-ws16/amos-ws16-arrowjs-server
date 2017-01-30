8238cc2e65beb174bdc70b1e80f26c5997b0d682
Kevin Marcel Styp-Rekowski <k.styp-rekowski@campus.tu-berlin.de>
2017-01-30 20:51:03 +0100

    added more test-cases for logging.js

--- test/logging-test.js
+++ test/logging-test.js
@@ -1,9 +1,10 @@
 const buster = require('buster')
 
 const logging = require('../lib/logging.js')
-// const sinon = require('sinon')
+const sinon = require('sinon')
 const Request = require('../lib/models/request')
-// const mongoose = require('mongoose')
+const packageInfoServer = require('../package.json')
+const packageInfoEngine = require('../node_modules/arrow/package.json')
 
 buster.testCase('request-database test', {
   setUp: function (done) {
@@ -11,8 +12,32 @@ buster.testCase('request-database test', {
     this.create = this.stub(Request, 'create')
     done()
   },
+
   'should call save function once': function () {
     logging.addRequest({}, {}, () => {})
     buster.assert.called(this.create)
+  },
+
+  'Should test, whether the right request and response gets saved': function () {
+    logging.addRequest('sample Request', 'sample Response', () => {})
+    buster.assert.calledWith(this.create, sinon.match.has('request', 'sample Request'))
+    buster.assert.calledWith(this.create, sinon.match.has('response', 'sample Response'))
+  },
+
+  'Should test, whether the right versions in the meta-information get saved': function () {
+    logging.addRequest('sample Request', 'sample Response', () => {})
+    buster.assert.calledWith(this.create, sinon.match.has(
+      'metaInfo', sinon.match.has('serverVersion', packageInfoServer.version))
+    )
+    buster.assert.calledWith(this.create, sinon.match.has(
+      'metaInfo', sinon.match.has('engineVersion', packageInfoEngine.version))
+    )
+  },
+
+  'Should test, whether a timestamp gets saved': function () {
+    logging.addRequest('sample Request', 'sample Response', () => {})
+    buster.assert.calledWith(this.create, sinon.match.has(
+      'metaInfo', sinon.match.has('timestamp', sinon.match.number))
+    )
   }
 })
